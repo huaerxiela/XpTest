bb861d53ec9a7f0249c9f5ab79d7e820ae3c9259
hexl <huaerxiela@gmail.com>
2023-11-26 18:38:44 +0800

    打造你自己的xposed-demo（三）

--- app/src/main/java/com/example/xptest/HookMain.java
+++ app/src/main/java/com/example/xptest/HookMain.java
@@ -1,12 +1,16 @@
 package com.example.xptest;
 
+import android.annotation.SuppressLint;
 import android.app.AndroidAppHelper;
+import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
+
+import java.lang.reflect.Method;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -29,9 +33,36 @@ public class HookMain implements IXposedHookLoadPackage {
                 new Runnable() {
                     @Override
                     public void run() {
-                        Context context = AndroidAppHelper.currentApplication().getApplicationContext();
-                        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+//                        Context context = AndroidAppHelper.currentApplication().getApplicationContext();
+                        Context context = getContext();
+                        if (context != null){
+                            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+                        }else {
+                            Log.i(TAG, "showToast run: context = nul");
+                        }
                     }
                 }, 3000);
+    }
+
+    @SuppressLint("PrivateApi")
+    private static Context getContext(){
+        Context context = null;
+        try {
+            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+            @SuppressLint("DiscouragedPrivateApi") Method currentActivityThreadMethod = activityThreadClass.getDeclaredMethod("currentActivityThread");
+            currentActivityThreadMethod.setAccessible(true);
+
+            Object activityThreadObj = currentActivityThreadMethod.invoke(null);
+
+            Method currentApplicationMethod = activityThreadClass.getDeclaredMethod("currentApplication");
+            currentApplicationMethod.setAccessible(true);
+            Application currentApplication = (Application) currentApplicationMethod.invoke(activityThreadObj);
+
+            assert currentApplication != null;
+            context = currentApplication.getApplicationContext();
+        } catch (Exception e) {
+            Log.e(TAG, "getContext: ", e);
+        }
+        return context;
     }
 }
